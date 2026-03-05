25b74fa7409fe2cf46c077d7f56ccb534f3fc823
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-03-05 20:45:12 +0530

    Today 05/03/2026  Element only Navigation

---
+++ L30.js/Q1.js
@@ -0,0 +1,18 @@
+const changeBgRed = ()=>{
+   document.body.firstElementChild.style.background = "pink";
+}
+let b = document.body
+changeBgRed();   
+console.log("First Child of b is : " , b.firstChild)
+console.log("First Child of b is : " , b.firstElementChild)
+
+
+// const changeBgRed = ()=>{
+//     document.body.firstElementChild.style.background = "red";
+// }
+
+// changeBgRed();   // function call
+
+// let b = document.body
+// console.log("First Child of b is : ", b.firstChild)
+// console.log("First Element Child of b is : ", b.firstElementChild)
