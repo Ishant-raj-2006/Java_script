c2511d8b640a1f758f5c4af85f97510c769e3f46
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-05-31 18:10:19 +0530

    js

--- OneSort.js/Array.js
+++ OneSort.js/Array.js
@@ -1,3 +1,23 @@
 // WAP for print all element of array 
 arr = [1,2,3,4,5,"Ishant"];
 console.log(arr);
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// add any Number in array in last 
+arr.push("Raj");
+console.log(arr);
+ // Delete the last elemrnt in array 
+ arr.pop();
+ console.log(arr);
+
+ // add somthing in array in fast
+ arr.unshift('Srivastav')
+ console.log(arr)
+
+ // Remove first element 
+ arr.shift();
+ console.log(arr);
+
+ 
