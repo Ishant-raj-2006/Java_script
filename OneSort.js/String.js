0782ddf3ff0972d217ae08f85289cc0fbd51d05d
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-06-05 11:04:56 +0530

    Today 05/06/2026

---
+++ OneSort.js/String.js
@@ -0,0 +1,35 @@
+// WAP for print your name by using a variable.
+let a = "Ishant";
+console.log(a);
+ 
+console.log("------------------------------------------------------------------");
+
+ 
+// WAP for print your name and it's length .
+console.log(a, a.length);
+
+console.log("------------------------------------------------------------------");
+
+
+// WAP for print your name by using for loop .
+for(let i=0; i<a.length; i++){
+    console.log(a[i]);
+}
+
+console.log("------------------------------------------------------------------");
+// WAP for print your name by using for of loop.
+for(const char of a){
+    console.log(char);
+}
+
+console.log("------------------------------------------------------------------");
+
+// WAP for convet all letters in capital latter .
+console.log(a.toUpperCase ())
+
+console.log("------------------------------------------------------------------");
+
+// WAP for convert all letters in small latters.
+console.log(a.toLowerCase())
+
+console.log("------------------------------------------------------------------");
