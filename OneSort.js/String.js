3158e63d05b70c191de4f010af8b94af54e3f2b9
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-06-05 11:26:48 +0530

    Today update 1.1

--- OneSort.js/String.js
+++ OneSort.js/String.js
@@ -33,3 +33,33 @@ console.log("------------------------------------------------------------------"
 console.log(a.toLowerCase())
 
 console.log("------------------------------------------------------------------");
+
+// WAP for remove unwanted space in any strinng .
+let b =       "                 Hello   Ishant            ";
+console.log(b.length);
+tri = b.trim();
+console.log(tri);
+console.log(tri.length);
+
+
+console.log("------------------------------------------------------------------");
+
+// WAP for find the index number of o in b after trime .
+console.log(b.indexOf('o'));
+
+// WAP for find the sub string between index number 5 to 28 in b.
+console.log("------------------------------------------------------------------");
+console.log(b.slice(5,28));
+
+
+// WAP for print somthing in Array just like "Hello Ishant who are you ".
+
+console.log("------------------------------------------------------------------");
+let name = "Hello Ishant who are you";
+console.log(name);
+console.log("------------------------------------------------------------------");
+
+let arr = name.split(' ');
+for(const ele of arr){
+    console.log(ele);
+}
