660738cbf7a27bcb18df8f27afdd4887255a7f45
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-06-07 13:57:27 +0530

    07/06/2026

---
+++ OneSort.js/Template.js
@@ -0,0 +1,5 @@
+let age = 20;
+let exp = 4;
+console.log("My age is ",age ," and my exprince is ",exp);
+console.log("My age is "+age +" and my exprince is "+exp);
+console.log(`My age is ${age}  and my exprince is ${exp}`);
