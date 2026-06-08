9ca31f6a085d173b1b221fee3cbafa79e34fa8f7
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-06-08 17:15:47 +0530

    This is java scrpit

---
+++ OneSort.js/Object.js
@@ -0,0 +1,21 @@
+// Object ht Kind of like math dictionary Where we have "Key-Value " pairs.
+var details = ["Ishant ", 22,72];
+console.log(details);
+
+// this is Object  
+let x = {
+    name: "Ishant",
+    age: 21,
+    percentage : 72.00,
+    IsMarried: false
+};
+console.log(x);
+
+// key me space ho skata h but single coat ya dual coat me hona chahiye string name 
+let y = {
+    'what is your name': "Ishant",
+    age: 21,
+    percentage : 72.00,
+    IsMarried: false
+};
+console.log(y);
